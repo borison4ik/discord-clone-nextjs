6c60fa3e3ca87ca2d4ba55b6ee9cd92a076221ad
Borisenko Valentin <ext-vborisenko@ozon.ru>
2024-10-17 01:00:03 +0700

    added messages page

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,8 @@
+import { redirect } from 'next/navigation';
 import { ChatHeader } from '@/components/chat/chat-header';
+import { ChatInput } from '@/components/chat/ChatInput';
 import { currentProfile } from '@/lib/current-profile';
 import { db } from '@/lib/db';
-import { redirect } from 'next/navigation';
 
 interface ChannelIdPageProps {
   params: {
@@ -37,6 +38,13 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
   return (
     <div className='bg-white dark:bg-[#323338] flex flex-col h-full'>
       <ChatHeader name={channel.name} serverId={channel.serverId} type='channel' />
+      <div className='flex-1'>featyre messages</div>
+      <ChatInput
+        name={channel.name}
+        type='channel'
+        apiUrl='/api/socket/messages'
+        query={{ channelId: channel.id, serverId: channel.serverId }}
+      />
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import './globals.css';
 import { cn } from '@/lib/utils';
 import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
@@ -5,7 +6,7 @@ import { ClerkProvider } from '@clerk/nextjs';
 
 import { ThemeProvider } from '@/components/providers/theme-provider';
 import { ModalProvider } from '@/components/providers/modal-provider';
-import './globals.css';
+import { SocketProvider } from '@/components/providers/socket-provider';
 
 const font = Open_Sans({ subsets: ['latin'] });
 
@@ -23,9 +24,15 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang='en' suppressHydrationWarning>
         <body className={cn(font.className, 'bg-white dark:bg-[#313338]', 'antialiased')}>
-          <ThemeProvider attribute='class' defaultTheme='dark' enableSystem={false} storageKey='discord-theme'>
-            <ModalProvider />
-            {children}
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='dark'
+            enableSystem={false}
+            storageKey='discord-theme'>
+            <SocketProvider>
+              <ModalProvider />
+              {children}
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </html>
