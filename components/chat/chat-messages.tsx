dfe063dbc2e99bd84cb38421dbfa9a22fc65a18c
Borisenko Valentin <ext-vborisenko@ozon.ru>
2024-10-20 18:26:48 +0700

    added tanstak query

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,6 +1,9 @@
 'use client';
 
 import { Member } from '@prisma/client';
+import { Loader2, ServerCrash } from 'lucide-react';
+
+import { useChatQuery } from '@/hooks/use-chat-query';
 
 import { ChatWelcome } from './chat-welcome';
 
@@ -26,6 +29,34 @@ export const ChatMessages = ({
   paramValue,
   type,
 }: ChatMessagesProps) => {
+  const queryKey = `chat:${chatId}`;
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isFetching, status } = useChatQuery(
+    {
+      queryKey,
+      apiUrl,
+      paramKey,
+      paramValue,
+    },
+  );
+
+  if (isFetching) {
+    return (
+      <div className='flex-1 flex flex-col py-4 items-center justify-center'>
+        <Loader2 className='h-7 w-7 text-zinc-500 animate-spin my-4' />
+        <p className='text-zinc-500 trxt-xs dark:text-zinc-400'>Loading...</p>
+      </div>
+    );
+  }
+
+  if (status === 'error') {
+    return (
+      <div className='flex-1 flex flex-col py-4 items-center justify-center'>
+        <ServerCrash className='h-7 w-7 text-zinc-500 my-4' />
+        <p className='text-zinc-500 trxt-xs dark:text-zinc-400'>Something went wrong!</p>
+      </div>
+    );
+  }
+
   return (
     <div className='flex-1 flex flex-col py-4 overflow-y-auto'>
       <div className='flex-1' />
